05f75e533b9ed4e5a6fecb341035136a4d0cc364
Md.Maknun Nehal <Nehal2048@gmail.com>
2024-03-15 15:24:24 +0600

    Submission

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-// import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
+import Countdown from './Countdown';
+import { useState } from 'react';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import dayjs from 'dayjs';
+import { renderTimeViewClock } from '@mui/x-date-pickers/timeViewRenderers';
 
 function App() {
+  const [selectedDate, setDate] = useState(new Date())
+
+  const handleDateChange = (newDate) => {
+    setDate(newDate);
+  };
+
   return (
-    <div className="App">
-      <h1>This is the home page.</h1>
-      <p>You should have a date picker here followed by a button to navigate to "/countdown" page</p>
-      <h3>Good Luck!</h3>
-      {/* <Link>Go to Countdown</Link> */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <div className="App">
+            <h1 className="title" >BALENCIAGA<br/>WINTER 24 COLLECTION</h1>
+            <h3>Choose a target date and time to start countdown</h3>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DateTimePicker
+                  className="dateTimePicker"
+                  value={dayjs(selectedDate)}
+                  onChange={handleDateChange}
+                  renderInput={(props) => <input {...props} />}
+                  viewRenderers={{
+                    hours: renderTimeViewClock,
+                    minutes: renderTimeViewClock,
+                    seconds: renderTimeViewClock,
+                  }}
+                />
+            </LocalizationProvider>
+            <div className="spacerLarge"></div>
+            <Link to="/countdown">
+              <button className='button'>Start Countdown</button>
+            </Link>
+          </div>
+        } />
+        <Route path="/countdown" element={<Countdown targetDate= {selectedDate} />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Countdown.js
+++ src/Countdown.js
@@ -1,9 +1,117 @@
 
-import './Countdown.css';
+import React, { useState, useEffect } from 'react';
 
-function Countdown() {
+function Countdown({targetDate}) {
+  const [isLoading, setLoading] = useState(true);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hours, setHours] = useState(0);
+
+  useEffect(() => {
+    //Loading Delay: Implement a 5-second loading delay before displaying the countdown page, marked by a loading spinner. (The spinner can be of any design, but it should be visible to the user for 5 seconds, typically encompassing the whole page with a central loading animation.)
+    const loadingTimer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    
+    const intervalId = setInterval(() => {
+      const timeRemaining = targetDate - new Date();
+
+      if (timeRemaining <= 0) {
+        setSeconds(0);
+        setMinutes(0);
+        setHours(0);
+      } else{
+        
+        const secondsR = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+        const minutesR = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const hoursR = Math.floor(timeRemaining / (1000 * 60 * 60));
+
+        //Update Timer Every Second
+        setSeconds(secondsR);
+        setMinutes(minutesR);
+        setHours(hoursR);
+      }
+    }, 1000);
+    
+    return () => {
+      clearInterval(loadingTimer);
+      clearInterval(intervalId);
+    };
+  }, [])
+
+  const formatDate = (targetDate) => {
+    const date = new Date(targetDate);
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+    const day = date.getDate()
+    let suffix = `${day}th`
+
+    if (!(day >= 11 && day <= 13)) {
+      switch (day % 10) {
+        case 1: 
+          suffix = `${day}st`;
+          break;
+        case 2: 
+          suffix = `${day}nd`;
+          break;
+        case 3:
+          suffix = `${day}rd`;
+          break;
+        default: 
+          suffix = `${day}th`;
+          break;
+      }
+    }
+    return `${monthNames[date.getMonth()]} ${suffix}, ${date.getFullYear()}`;
+  };
+
+  const formatTimerNum = (num) => {
+    return num >= 10 ? `${num}` : `0${num}`;
+  }
+
   return (
     <div className="Countdown">
+      {isLoading ? (
+        <div className="loading-container">
+        <div className="loader"></div>
+        <h2 className="title">Please Wait...</h2>
+      </div>
+      ) : (
+        <div>
+          <h1 className="title">BALENCIAGA<br/>WINTER 24 COLLECTION</h1>
+          <div className="timer">
+            <div className="timer-item">
+              <div className="timer-number">{formatTimerNum(hours)}</div>
+              <div className="timer-label">HOURS</div>
+            </div>
+
+            <div className="colon">
+              <div>:</div>
+              <div className="spacer"></div>
+            </div>
+
+            <div className="timer-item">
+              <div className="timer-number">{formatTimerNum(minutes)}</div>
+              <div className="timer-label">MINUTES</div>
+            </div>
+
+            <div className="colon">
+              <div>:</div>
+              <div className="spacer"></div>
+            </div>
+
+            <div className="timer-item">
+              <div className="timer-number">{formatTimerNum(seconds)}</div>
+              <div className="timer-label">SECONDS</div>
+            </div>
+
+          </div>
+          <div className="date-text">{formatDate(targetDate)}</div>
+        </div>
+      )}
+      
     </div>
   );
 }
